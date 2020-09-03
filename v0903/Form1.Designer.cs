2e1478a99b8f2ba02ca2002b0c4404205ae169b6
nozawa <DASchool.12@gmail.com>
2020-09-03 09:32:33 +0900

    完成

--- v0903/Form1.Designer.cs
+++ v0903/Form1.Designer.cs
@@ -41,12 +41,18 @@
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(351, 393);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
--- v0903/Form1.cs
+++ v0903/Form1.cs
@@ -12,9 +12,46 @@ namespace v0903
 {
     public partial class Form1 : Form
     {
+        int vx = rand.Next(-10, 11);
+        int vy = rand.Next(-10, 11);
+        int point = -100;
+        static Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Height - label1.Height);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
         }
     }
 }
